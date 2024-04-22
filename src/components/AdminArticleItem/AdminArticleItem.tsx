783aa29c64f488c705e157d6959cd6ef8464e810
Atabegovv <atabegov98@mail.ru>
2024-04-22 10:23:53 +0300

    form validation

--- src/components/AdminArticleItem/AdminArticleItem.tsx
+++ src/components/AdminArticleItem/AdminArticleItem.tsx
@@ -1,34 +1,171 @@
-import React, { FC } from 'react';
+import React, { ChangeEvent, FC, FormEvent, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import { Typography } from '@mui/material';
 import { CardActionArea } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { getErrors, getImage } from './helpers';
+import { InputErrors, InputName, InputRefs, InputValues } from './types';
 
 export const AdminArticleItem: FC = () => {
     const { id }: { id?: string } = useParams();
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const inputRefs: InputRefs = {
+        'company-name': useRef<HTMLInputElement>(),
+        title: useRef<HTMLInputElement>(),
+        description: useRef<HTMLTextAreaElement>(),
+        text: useRef<HTMLTextAreaElement>(),
+        image: useRef<HTMLInputElement>(),
+    };
+    const [inputFile, setInputFile] = useState<File | null>(null);
+    const [inputErrors, setInputErrors] = useState<InputErrors>({
+        'company-name': '',
+        title: '',
+        description: '',
+        text: '',
+        image: '',
+    });
+    const [inputValues, setInputValues] = useState<InputValues>({
+        'company-name': '',
+        title: '',
+        description: '',
+        text: '',
+        image: '',
+    });
+    const onChangeInput = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const input = event.currentTarget;
+        const name = input.name;
+        const value = input.value;
+
+        setInputValues({
+            ...inputValues,
+            [name]: value,
+        });
+    };
+    const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        // 1. Собрать данные
+        const data = new FormData();
+
+        Object.entries(inputValues).forEach(([name, value]) => {
+            if (name === 'image') {
+                data.append(name, inputFile || new File([], ''));
+            } else {
+                data.append(name, value);
+            }
+        });
+
+        // 2. Проверить данные
+        const errors = await getErrors(Array.from(data.entries()) as [InputName, FormDataEntryValue][]);
+        const errorsEntries = Object.entries(errors);
+
+        // 3.1 Подстветить ошибки
+        setInputErrors(errors);
+
+        // 3.2 Сфокусироваться на первом ошибочном инпуте
+        const errorInput = errorsEntries.find(([, value]) => value.length > 0);
+
+        if (errorInput) {
+            const name = errorInput[0] as InputName;
+            const inputRef = inputRefs[name];
+
+            if (inputRef.current) {
+                inputRef.current.focus();
+            }
+
+            return;
+        }
+
+        // 4. Если все ок, отправить данные
+        fetch('https://httpbin.org/post', {
+            method: 'POST',
+            body: data,
+        });
+    };
+
+    const showFile = (event: ChangeEvent<HTMLInputElement>) => {
+        const files = event.currentTarget.files;
+
+        if (files === null || !files.length) {
+            return;
+        }
+
+        const file = files[0];
+
+        if (file.size === 0 || !file.type.startsWith('image/')) {
+            return;
+        }
+
+        setInputFile(file);
+
+        getImage(file).then((image) => {
+            setInputValues({
+                ...inputValues,
+                image: image.src,
+            });
+        });
+    };
+
     return (
-        <>
-            <Grid container spacing={2}>
-                <Grid item xs={7}>
+        <form noValidate onSubmit={onSubmit}>
+            <Grid container spacing={2} sx={{ mb: 3 }}>
+                <Grid item xs={9}>
                     <Typography variant="h4" gutterBottom>
-                        {id ? 'Как сдалать' : 'Новая статья'}
+                        {id ? 'Редактирование статьи «Как мыть руки правильно»' : 'Новая статья'}
                     </Typography>
                 </Grid>
                 <Grid item xs={3}>
-                    <Box sx={{ display: 'flex', justifyContent: 'right' }}>
-                        <Button variant="contained" color="success">
+                    <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                        <Button type="submit" variant="contained" color="success" sx={{ mr: 1 }}>
                             Сохранить
                         </Button>
+
                         {id && (
-                            <Button variant="outlined" color="error" sx={{ marginLeft: '10px' }}>
-                                Удалить
-                            </Button>
+                            <div>
+                                <IconButton
+                                    aria-label="more"
+                                    id="long-button"
+                                    aria-controls="long-menu"
+                                    aria-expanded={open ? 'true' : undefined}
+                                    aria-haspopup="true"
+                                    onClick={handleClick}
+                                >
+                                    <MoreVertIcon />
+                                </IconButton>
+                                <Menu
+                                    id="long-menu"
+                                    MenuListProps={{
+                                        'aria-labelledby': 'long-button',
+                                    }}
+                                    anchorEl={anchorEl}
+                                    open={open}
+                                    onClose={handleClose}
+                                >
+                                    <MenuItem onClick={handleClose}>
+                                        <Button variant="outlined" color="error">
+                                            Удалить статью
+                                        </Button>
+                                    </MenuItem>
+                                </Menu>
+                            </div>
                         )}
                     </Box>
                 </Grid>
@@ -38,35 +175,85 @@ export const AdminArticleItem: FC = () => {
                 <Grid item xs={7}>
                     <Grid container spacing={2}>
                         <Grid item xs={12}>
-                            <TextField fullWidth label="Компания" id="fullWidth" />
+                            <TextField
+                                fullWidth
+                                label="Компания"
+                                variant="outlined"
+                                name="company-name"
+                                value={inputValues['company-name']}
+                                onChange={onChangeInput}
+                                ref={inputRefs['company-name']}
+                                error={Boolean(inputErrors['company-name'].length)}
+                                helperText={inputErrors['company-name']}
+                            />
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField fullWidth label="Название статьи" id="fullWidth" />
+                            <TextField
+                                fullWidth
+                                label="Название статьи"
+                                variant="outlined"
+                                name="title"
+                                value={inputValues.title}
+                                onChange={onChangeInput}
+                                ref={inputRefs.title}
+                                error={Boolean(inputErrors.title.length)}
+                                helperText={inputErrors.title}
+                            />
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField fullWidth label="Подводка" id="fullWidth" multiline maxRows={3} />
+                            <TextField
+                                fullWidth
+                                multiline
+                                maxRows={4}
+                                label="Подводка"
+                                variant="outlined"
+                                name="description"
+                                value={inputValues.description}
+                                onChange={onChangeInput}
+                                ref={inputRefs.description}
+                                error={Boolean(inputErrors.description.length)}
+                                helperText={inputErrors.description}
+                            />
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField fullWidth label="Техст" id="fullWidth" multiline rows={3} />
+                            <TextField
+                                fullWidth
+                                multiline
+                                rows={4}
+                                maxRows={12}
+                                label="Текст"
+                                variant="outlined"
+                                name="text"
+                                value={inputValues.text}
+                                onChange={onChangeInput}
+                                ref={inputRefs.text}
+                                error={Boolean(inputErrors.text.length)}
+                                helperText={inputErrors.text}
+                            />
                         </Grid>
                     </Grid>
                 </Grid>
 
-                <Grid item xs={3}>
+                <Grid item xs={5}>
                     <Card>
                         <CardActionArea>
-                            <CardMedia
-                                component="img"
-                                image="https://placehold.co/600x400?text=Hello+World"
-                                alt="green iguana"
-                            />
+                            <CardMedia component="img" height="140" image={inputValues.image} />
                             <CardContent>
-                                <input type={'file'} />
+                                <TextField
+                                    type="file"
+                                    name="image"
+                                    fullWidth
+                                    variant="outlined"
+                                    onChange={showFile}
+                                    ref={inputRefs.image}
+                                    error={Boolean(inputErrors.image.length)}
+                                    helperText={inputErrors.image}
+                                />
                             </CardContent>
                         </CardActionArea>
                     </Card>
                 </Grid>
             </Grid>
-        </>
+        </form>
     );
 };
